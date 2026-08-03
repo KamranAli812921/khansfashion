0280274ba28a80ceb780ea88b15c2e31b56f0213
Kamran Ali <kamranroonjha94@gmail.com>
2026-08-03 21:50:03 +0500

    Add TLS rejectUnauthorized false option to bypass cloud certificate trust verification

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -19,6 +19,9 @@ const sendEmail = async ({ to, subject, text, html }) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
 
